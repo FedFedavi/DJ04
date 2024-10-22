11d841195e2311159825f59d14eea522d260ed41
FedFedavi <kykypyk@gmail.com>
2024-10-22 19:29:37 +0400

    add menu & footer

--- movie_project/films/forms.py
+++ movie_project/films/forms.py
@@ -6,3 +6,8 @@ class FilmForm(forms.ModelForm):
     class Meta:
         model = Film
         fields = ['title', 'description', 'review']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'review': forms.Textarea(attrs={'class': 'form-control'}),
+        }
